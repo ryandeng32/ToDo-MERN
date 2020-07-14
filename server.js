efc01db61864b1300211a2ce3ef83a3c655504a4
Ryan Deng <ryan.deng32@gmail.com>
2020-07-14 12:15:36 -0400

    install axios and change index.html

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Hello");
 });
-
-PORT = 3000;
+app.post("/", (req, res) => {
+  console.log(req.body);
+});
+PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
 });
